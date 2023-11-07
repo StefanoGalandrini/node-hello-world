1360a36ca788ecbc4c89f9acdad98d9fb64efd5b
Stefano <stefe68@gmail.com>
2023-11-07 15:00:15 +0100

    Add server configuration to server.js

--- server.js
+++ server.js
@@ -1,6 +1,46 @@
-// server.js
+// server.js init libraries
 
 const http = require("http");
 const dotenv = require("dotenv");
 
-dotenv.config(); // Carica le variabili d'ambiente
+dotenv.config(); // start dotenv library
+
+
+/**
+ * Server port type
+ * @type {number}
+ */
+
+let port = +process.env.PORT || 3000;
+
+/**
+ * Request function to send HTML to server 
+ * @param {http.ServerResponse} res - L'oggetto response del server.
+ * @param {string} content - Il contenuto HTML da inviare.
+ */
+function htmlResponse(res, content)
+{
+	res.writeHead(200, { "Content-Type": "text/html" });
+	res.end(content);
+}
+
+
+/**
+ * Create server and callback function
+ * @param {http.ClientRequest} req
+ * @param {http.ServerResponse} res
+ */
+const server = http.createServer(function (req, res)
+{
+	// request variable to display
+
+	const reqVar = `<h1>Variabile richiesta: ${process.env.RESPONSE_VAR}</h1>`;
+
+	htmlResponse(res, reqVar);
+});
+
+// Start server
+server.listen(port, function ()
+{
+	console.log("Server is running on http://localhost:" + port);
+});
