1efb37321ca3a184dffb348255eca71b84495fb9
Stefano <stefe68@gmail.com>
2023-11-07 17:17:58 +0100

    Move sentences in other file - data.js

---
+++ data.js
@@ -0,0 +1,19 @@
+/**
+ * Array of sentences to display
+ * @type {string[]}
+ */
+const randomSentences = [
+	"Il successo non è definitivo, il fallimento non è fatale: è il coraggio di continuare che conta.",
+	"Anche il viaggio più lungo inizia con un solo passo.",
+	"Cambia i tuoi pensieri e cambierai il tuo mondo.",
+	"La vita non consiste nel trovare te stesso. La vita consiste nel creare te stesso.",
+	"Vivi come se dovessi morire domani. Impara come se dovessi vivere per sempre.",
+	"Credi di poterlo fare e sei già a metà strada.",
+	"Non aspettare. Il tempo non sarà mai giusto.",
+	"Non è la montagna che conquistiamo, ma noi stessi.",
+	"Sii te stesso; tutti gli altri sono già presi.",
+	"La felicità non è qualcosa di già pronto. Viene dalle tue azioni.",
+];
+
+
+module.exports = randomSentences;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const http = require("http");
 const dotenv = require("dotenv");
+const randomSentences = require('./data.js');
 
 dotenv.config(); // start dotenv library
 
@@ -15,31 +16,14 @@ let port = +process.env.PORT || 3000;
 
 /**
  * Request function to send HTML to server 
- * @param {http.ServerResponse} res - L'oggetto response del server.
- * @param {string} content - Il contenuto HTML da inviare.
+ * @param {http.ServerResponse} res
+ * @param {string} content
  */
 function htmlResponse(res, content)
 {
 	res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
 	res.end(content);
 }
-
-/**
- * Array of sentences to display
- * @type {string[]}
- */
-const randomSentences = [
-	"Il successo non è definitivo, il fallimento non è fatale: è il coraggio di continuare che conta.",
-	"Anche il viaggio più lungo inizia con un solo passo.",
-	"Cambia i tuoi pensieri e cambierai il tuo mondo.",
-	"La vita non consiste nel trovare te stesso. La vita consiste nel creare te stesso.",
-	"Vivi come se dovessi morire domani. Impara come se dovessi vivere per sempre.",
-	"Credi di poterlo fare e sei già a metà strada.",
-	"Non aspettare. Il tempo non sarà mai giusto.",
-	"Non è la montagna che conquistiamo, ma noi stessi.",
-	"Sii te stesso; tutti gli altri sono già presi.",
-	"La felicità non è qualcosa di già pronto. Viene dalle tue azioni.",
-];
 
 /**
  * Array to keep track of already shown sentences
@@ -67,6 +51,7 @@ function getRandomSentence()
 	} while (shownSentences.includes(index));
 
 	shownSentences.push(index);
+	console.log(shownSentences);
 	return randomSentences[index];
 }
 
