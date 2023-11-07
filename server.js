539e8f33ac7d2938232338fda6a133de89dd783e
Stefano <stefe68@gmail.com>
2023-11-07 17:09:38 +0100

    Add more sentences

--- server.js
+++ server.js
@@ -29,11 +29,16 @@ function htmlResponse(res, content)
  * @type {string[]}
  */
 const randomSentences = [
-	"La perseveranza è ciò che rende l’impossibile possibile, il possibile probabile e il probabile certo.",
-	"La vita è un 10% cosa ti accade e un 90% come reagisci.",
+	"Il successo non è definitivo, il fallimento non è fatale: è il coraggio di continuare che conta.",
+	"Anche il viaggio più lungo inizia con un solo passo.",
+	"Cambia i tuoi pensieri e cambierai il tuo mondo.",
+	"La vita non consiste nel trovare te stesso. La vita consiste nel creare te stesso.",
+	"Vivi come se dovessi morire domani. Impara come se dovessi vivere per sempre.",
+	"Credi di poterlo fare e sei già a metà strada.",
 	"Non aspettare. Il tempo non sarà mai giusto.",
-	"Sii il cambiamento che vuoi vedere nel mondo.",
-	"La miglior visione è l’intuizione."
+	"Non è la montagna che conquistiamo, ma noi stessi.",
+	"Sii te stesso; tutti gli altri sono già presi.",
+	"La felicità non è qualcosa di già pronto. Viene dalle tue azioni.",
 ];
 
 /**
